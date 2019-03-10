5d7dff1dba7e669e3a213beaceed447ff570d26b
Mário Ferreira <marioferreira2110@gmail.com>
2019-03-10 20:59:59 +0100

    Signed-off-by: Mário Ferreira <marioferreira2110@gmail.com>
    usage function - usage renamed as usage_message (this caused segmentation fault)
    
    Check if the ip address was entered by the user
    Check if the ip address is a valid one

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,17 +7,16 @@
 #include <regex.h>
 #include <sys/socket.h>
 
-#include "miscellanous.h"
+#include "misc.h"
 
-void usage(FILE* std) {
-	char* message = "Usage %s\n"
-			"-a [--address]\t\tAdress to scan\n"
+void usage(FILE *std) {
+	char *usage_message = "Usage %s\n"
+			"-a [--address]\t\tAdress to scan - from 0.0.0.0 to 255.255.255.255\n"
 			"-h [--help]\t\tDisplay this message\n"
-			"-m [--min]\t\tMin value of the ports range to scan\n"
-			"-M [--max]\t\tMax value of the ports range to scan\n";
+			"-m [--min]\t\tMin value of the ports range to scan (min = 1)\n"
+			"-M [--max]\t\tMax value of the ports range to scan (up to %d)\n";
 
-	fprintf(std, message, PROGRAM_NAME);
-
+	fprintf(std, usage_message, PROGRAM_NAME, MAX_PORT);
 }
 
 
@@ -28,11 +29,17 @@ int main (int argc, char* argv[]) {
 	int option = 0;
 	int option_index = 0;
 
-	char* address = NULL;
-	char* message = NULL;
+	char *address = NULL;
+	char *message = NULL;
 	// Min and max before being converted to int, this allows to check if they match the regex(only strings)
-	char* cl_min = NULL;
-	char* cl_max = NULL;
+	// cl_min = mix value entered from the command line
+	char *cl_min = NULL;
+	char *cl_max = NULL;
+
+	size_t nmatch = 4;
+
+	regex_t preg;
+	regmatch_t pmatch[nmatch];
 
 	struct option long_options[] = {
 		{"address",	required_argument,	0, 'a'},
@@ -50,7 +57,7 @@ int main (int argc, char* argv[]) {
 		goto END;
 	}
 
-	while ((option = getopt_long(argc, argv, "a:m:M:", long_options, &option_index)) != -1) {
+	while ((option = getopt_long(argc, argv, "a:hm:M:", long_options, &option_index)) != -1) {
 		switch (option) {
 			case 'a':
 				if ((address = strdup(optarg)) == NULL) {
@@ -97,6 +104,14 @@ int main (int argc, char* argv[]) {
 				break; // paranoia
 		}
 	}
+
+	// If address was not entered by the user
+	if (address == NULL) {
+		usage(stderr);
+		status = 1;
+
+		goto END;
+	}
 	
 	// Min not passed as an argument
 	if (cl_min == NULL) {
@@ -108,7 +123,34 @@ int main (int argc, char* argv[]) {
 		max = MAX_PORT;
 	}
 
+	if ((regcomp(&preg, ADDRESS_REGEX, REG_EXTENDED)) != 0) {
+		if (asprintf(&message, "Failed to compile ADDRESS_REGEX\n") > 0) {
+			fprintf(stderr, message);
+		}
+
+		status = 1;
+		goto END;
+	}
+
+	if ((regexec(&preg, address, nmatch, pmatch, 0)) != 0) {
+		if (asprintf(&message, "%s is not a valid ip address\n", address) > 0) {
+			fprintf(stderr, message);
+		}
+
+		status = 1;
+		goto END;
+	}
+
+	fprintf(stdout, "Your ip address : %s\n", address);
+	regfree(&preg);
+
+	// Allows to free memory without having a mess(free() and return everywhere)
 	END:
+
+	if (message != NULL) {
+		free(message);
+		message = NULL;
+	}
 
 	if (address != NULL) {
 		free(address);
